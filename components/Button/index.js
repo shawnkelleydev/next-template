6f0eeb6baa0110566a4aa41bd850bc5ee8c2d728
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-26 21:23:41 -0600

    Delete Button

--- components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css'
-
-export default function Button({ children, className, ...rest }) {
-  return (
-    <button className={`${styles.button} ${className}`} {...rest}>
-      {children}
-    </button>
-  )
-}
